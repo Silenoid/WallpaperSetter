0389dc111fdfba237124813805e4658220a9cd52
Vincenzo <city17@hotmail.it>
2020-06-05 11:41:40 +0200

    Randomized returned graphic elements

--- src/Art/GraphicEntities/ElementFactory.cs
+++ src/Art/GraphicEntities/ElementFactory.cs
@@ -1,14 +1,24 @@
-﻿using wallpaperSetter.Art.GraphicEntities.BigElements;
+﻿using System;
+using wallpaperSetter.Art.GraphicEntities.BigElements;
 using wallpaperSetter.Art.GraphicEntities.SmallElements;
+using wallpaperSetter.Utilities;
 
 namespace wallpaperSetter.Art.GraphicEntities {
 	public class ElementFactory {
+		
 		public static IElement getRendomBigElement() {
-			return new StarFrame();
+			switch (GenericUtils.random.Next(1)) {
+				case 0: return new StarFrame();
+				default: throw new NotImplementedException("There is no graphic object assigned to this case");
+			}
 		}
 
 		public static IElement getRendomSmallElement() {
-			return new AmebaParticles();
+			switch (GenericUtils.random.Next(2)) {
+				case 0: return new AmebaParticles();
+				case 1: return new Mimmolini();
+				default: throw new NotImplementedException("There is no graphic object assigned to this case");
+			}
 		}
 	}
 }
